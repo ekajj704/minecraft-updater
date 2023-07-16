d53ebc375374110a59d77c6fd7f0bf1d37a57027
ekajj704 <120057827+ekajj704@users.noreply.github.com>
2023-07-15 17:28:31 -0700

    Added script execution

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ async function main(){
     await page.goto("https://www.minecraft.net/en-us/download/server/bedrock");
     let html = page.getByLabel("Download Minecraft Dedicated Server software for Ubuntu (Linux)");
     let link = await html.getAttribute("href");
-    console.log(link);
     browser.close();
+    exec(`../updateBedrock.sh '${link}'`);
 }
 
 main();
